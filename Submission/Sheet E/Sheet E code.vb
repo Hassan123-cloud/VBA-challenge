fe15b3ffcb7f72426c963795c3576990b27610ca
Hassan123-cloud <52514825+Hassan123-cloud@users.noreply.github.com>
2024-05-09 19:42:47 -0500

    VBA Submission

--- Submission/Sheet E/Sheet E code.vb
+++ Submission/Sheet E/Sheet E code.vb
@@ -5,7 +5,8 @@ Sub stocks():
     Dim cell_vol As LongLong ' contents of column G (cell volume)
     Dim vol_total As LongLong ' what is going to go in column L
     Dim ticker As String ' what is going to go in column I
-
+    Dim ws As Worksheet
+    
     Dim k As Long ' leaderboard row
     
     Dim ticker_close As Double
@@ -15,21 +16,22 @@ Sub stocks():
     
     ' asked the xpert
     Dim lastRow As Long
+    For Each ws In ThisWorkbook.Worksheets
     lastRow = ActiveSheet.Cells(ActiveSheet.Rows.Count, 1).End(xlUp).Row
 
     vol_total = 0
     k = 2
     
     ' Write Leaderboard Columns
-    Range("I1").Value = "Ticker"
-    Range("J1").Value = "Quarterly Change"
-    Range("K1").Value = "Percent Change"
-    Range("L1").Value = "Volume"
-    Range("O2").Value = "Greatest % Increase"
-    Range("O3").Value = "Greatest % Decrease"
-    Range("O4").Value = "Greatest Total Volume"
-    Range("P1").Value = "Ticker"
-    Range("Q1").Value = "Value"
+    ws.Range("I1").Value = "Ticker"
+    ws.Range("J1").Value = "Quarterly Change"
+    ws.Range("K1").Value = "Percent Change"
+    ws.Range("L1").Value = "Volume"
+    ws.Range("O2").Value = "Greatest % Increase"
+    ws.Range("O3").Value = "Greatest % Decrease"
+    ws.Range("O4").Value = "Greatest Total Volume"
+    ws.Range("P1").Value = "Ticker"
+    ws.Range("Q1").Value = "Value"
     
     
     
@@ -37,11 +39,11 @@ Sub stocks():
     
     
     ' assign open for first ticker
-    ticker_open = Cells(2, 3).Value
+    ticker_open = ws.Cells(2, 3).Value
 
     For i = 2 To lastRow:
-        cell_vol = Cells(i, 7).Value
-        ticker = Cells(i, 1).Value
+        cell_vol = ws.Cells(i, 7).Value
+        ticker = ws.Cells(i, 1).Value
 
         ' LOOP rows 2 to 254
         ' check if next row ticker is DIFFERENT
@@ -49,12 +51,12 @@ Sub stocks():
         ' if DIFFERENT, then we need add last row, write out to the leaderboard
         ' reset the vol_total to 0
 
-        If (Cells(i + 1, 1).Value <> ticker) Then
+        If (ws.Cells(i + 1, 1).Value <> ticker) Then
             ' OMG we have a different ticker, panic
             vol_total = vol_total + cell_vol
             
             ' get the closing price of the ticker
-            ticker_close = Cells(i, 6).Value
+            ticker_close = ws.Cells(i, 6).Value
             price_change = ticker_close - ticker_open
             
             ' Check if open price is 0 first - to protect from Kevin the Hacker
@@ -64,18 +66,18 @@ Sub stocks():
                 percent_change = 0
             End If
 
-            Cells(k, 9).Value = ticker
-            Cells(k, 10).Value = price_change
-            Cells(k, 11).Value = percent_change
-            Cells(k, 12).Value = vol_total
+            ws.Cells(k, 9).Value = ticker
+            ws.Cells(k, 10).Value = price_change
+            ws.Cells(k, 11).Value = percent_change
+            ws.Cells(k, 12).Value = vol_total
             
             ' formatting
             If (price_change > 0) Then
-                Cells(k, 10).Interior.ColorIndex = 4 ' Green
+                ws.Cells(k, 10).Interior.ColorIndex = 4 ' Green
             ElseIf (price_change < 0) Then
-                Cells(k, 10).Interior.ColorIndex = 3 ' Red
+                ws.Cells(k, 10).Interior.ColorIndex = 3 ' Red
             Else
-                Cells(k, 10).Interior.ColorIndex = 2 ' White
+                ws.Cells(k, 10).Interior.ColorIndex = 2 ' White
             End If
 
             ' reset
@@ -89,13 +91,13 @@ Sub stocks():
     Next i
     
     ' Style my leaderboard
-    Columns("K:K").NumberFormat = "0.00%"
-    Columns("I:L").AutoFit
+    ws.Columns("K:K").NumberFormat = "0.00%"
+    ws.Columns("I:L").AutoFit
     
     
-    Range("Q2") = "%" & WorksheetFunction.Max(Range("K2:K" & lastRow)) * 100
-    Range("Q3") = "%" & WorksheetFunction.Min(Range("K2:K" & lastRow)) * 100
-    Range("Q4") = "%" & WorksheetFunction.Max(Range("L2:L" & lastRow)) * 100
+    ws.Range("Q2") = "%" & WorksheetFunction.Max(Range("K2:K" & lastRow)) * 100
+    ws.Range("Q3") = "%" & WorksheetFunction.Min(Range("K2:K" & lastRow)) * 100
+    ws.Range("Q4") = WorksheetFunction.Max(Range("L2:L" & lastRow)) * 100
     
     Dim increase_number As Integer
     Dim decrease_number As Integer
@@ -106,9 +108,15 @@ Sub stocks():
     increase_number = WorksheetFunction.Match(WorksheetFunction.Max(Range("K2:K" & lastRow)), Range("K2:K" & lastRow), 0)
         decrease_number = WorksheetFunction.Match(WorksheetFunction.Min(Range("K2:K" & lastRow)), Range("K2:K" & lastRow), 0)
         volume_number = WorksheetFunction.Match(WorksheetFunction.Max(Range("L2:L" & lastRow)), Range("L2:L" & lastRow), 0)
-        Range("P2") = Cells(increase_number + 1, 9)
-        Range("P3") = Cells(decrease_number + 1, 9)
-        Range("P4") = Cells(volume_number + 1, 9)
+        ws.Range("P2") = Cells(increase_number + 1, 9)
+        ws.Range("P3") = Cells(decrease_number + 1, 9)
+        ws.Range("P4") = Cells(volume_number + 1, 9)
+        Next ws
 End Sub
+
+
+
+
+
 
 
